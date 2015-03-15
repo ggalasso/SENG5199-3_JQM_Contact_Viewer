ee221efaf3e341ee6ee65f423229aef13b6d1902
ggalasso <ggalasso@gmail.com>
2015-03-15 00:15:13 -0500

    Added way to add new contacts.
    Add new contacts to the contact viewer by performing a post request.

--- contact-viewer.js
+++ contact-viewer.js
@@ -2,7 +2,6 @@ var apiKey = 'bricked'
 var _contacts = {};
 var _contactID = null;
 
-console.log('loading');
 $(document).on("pagebeforeshow", "#home-page", function() {
 	var contactList = $('#contact-list')
 	contactList.html('')
@@ -13,9 +12,9 @@ $(document).on("pagebeforeshow", "#home-page", function() {
 		for (i in result.contacts) {
 			var contact = result.contacts[i]
 			_contacts[contact._id] = contact
-			contactList.append('<li><a href="#details-page" ' + 
-				'data-contact-id="' + 
-				contact._id + '">' 
+			contactList.append('<li><a href="#details-page" ' +
+				'data-contact-id="' +
+				contact._id + '">'
 				+ contact.name + '</a></li>')
 		};
 		contactList.listview('refresh')
@@ -39,4 +38,49 @@ $(document).on('pagebeforeshow', '#details-page', function() {
 	$('h2').append(contact.name)
 	//$('first_name').append(contact.name);
 
-})
+});
+
+$(document).on('pagebeforeshow', '#add-page', function() {
+	//Clear out all of the fields each time the page is loaded
+	$('#name').val("");
+	$('#title').val("");
+	$('#email').val("");
+	$('#phone').val("");
+	$('#twitter').val("");
+});
+
+$(document).on('submit', '#add-page', function(){
+	var myName = $('#name').val();
+	var myTitle = $('#title').val();
+	var myEmail = $('#email').val();
+	var myPhone = $('#phone').val();
+	var myTwitter = $('#twitter').val();
+	console.log('Contact Info:' + myName + ' ' + myTitle + myPhone)
+	//http://api.jquery.com/jQuery.ajax/
+	$.ajax({
+		url: 'http://contacts.tinyapollo.com/contacts/?key=' + apiKey,
+		method: 'POST',
+		data: {
+			name: myName,
+			title: myTitle,
+			email: myEmail,
+			phone: myPhone,
+			twitterID: myTwitter
+		},
+		//http://api.jquery.com/Ajax_Events/
+    complete: function() {
+    },
+    success: function (result) {
+      if(result.status) {
+					console.log('Result:' + result.status + ' Name:' + myName + ' Title:' + myTitle + ' Email:' + myEmail + ' Phone:' + myPhone + ' Twitter:' + myTwitter)
+					$.mobile.changePage("#home-page");
+      } else {
+					console.log('Contact Failed!')
+      }
+    },
+    error: function (request,error) {
+      // This callback function will trigger on unsuccessful action
+			console.log('Inside Error Function!' + error + request)
+		}
+	});
+});
