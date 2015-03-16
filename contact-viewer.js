067cb76c0823b8d2bb2bacb9e00ce1c0d15f8a35
PankajMotwani <motwa005@umn.edu>
2015-03-16 12:24:51 -0500

    Edit functionality uploaded

--- contact-viewer.js
+++ contact-viewer.js
@@ -2,6 +2,7 @@ var apiKey = 'bricked'
 var _contacts = {};
 var _contactID = null;
 
+
 $(document).on("pagebeforeshow", "#home-page", function() {
 	var contactList = $('#contact-list')
 	contactList.html('')
@@ -27,7 +28,6 @@ $(document).on('click', '#contact-list a', function() {
 	console.log("contact id is: " +  _contactID)
 	return true
 })
-
 
 $(document).on('pagebeforeshow', '#details-page', function() {
 	var contact = _contacts[_contactID]
@@ -57,7 +57,7 @@ $(document).on('click', '#delete-me', function(){
 			},
 			success: function (result) {
 			  if(result.status) {
-							console.log('Contact Deleted' + _contactID)
+							console.log('Contact Deleted: ' + _contactID)
 							$.mobile.changePage("#home-page");
 			  } else {
 							console.log('Delete Contact Failed!' + result);
@@ -73,47 +73,99 @@ $(document).on('click', '#delete-me', function(){
 	return true
 });
 
+
 $(document).on('pagebeforeshow', '#add-page', function() {
 	//Clear out all of the fields each time the page is loaded
-	$('#name').val("");
-	$('#title').val("");
-	$('#email').val("");
-	$('#phone').val("");
-	$('#twitter').val("");
+	if (_contactID == null)
+		{
+			$('#add-contact-form h1').text('Add Contact')
+			$('#name').val("");
+			$('#title').val("");
+			$('#email').val("");
+			$('#phone').val("");
+			$('#twitter').val("");
+			
+			console.log("add clicked ")
+		}
+	else
+		{ 
+			$('#add-contact-form h1').text('Edit Contact');
+			var contact = _contacts[_contactID];
+			$('#name').val(contact.name);
+			$('#title').val(contact.title);
+			$('#email').val(contact.email);
+			$('#phone').val(contact.phone);
+			$('#twitter').val(contact.twitterId);
+			
+			console.log("edit clicked " +  _contactID)
+		}
 });
 
 $(document).on('submit', '#add-page', function(){
-	var myName = $('#name').val();
-	var myTitle = $('#title').val();
-	var myEmail = $('#email').val();
-	var myPhone = $('#phone').val();
-	var myTwitter = $('#twitter').val();
+	var myName = $('#name').val().trim();
+	var myTitle = $('#title').val().trim();
+	var myEmail = $('#email').val().trim();
+	var myPhone = $('#phone').val().trim();
+	var myTwitter = $('#twitter').val().trim();
 	console.log('Contact Info:' + myName + ' ' + myTitle + myPhone)
 	//http://api.jquery.com/jQuery.ajax/
-	$.ajax({
-		url: 'http://contacts.tinyapollo.com/contacts/?key=' + apiKey,
-		method: 'POST',
-		data: {
-			name: myName,
-			title: myTitle,
-			email: myEmail,
-			phone: myPhone,
-			twitterID: myTwitter
+
+	
+	if(_contactID==null){
+		
+		$.ajax({
+			url: 'http://contacts.tinyapollo.com/contacts/?key=' + apiKey,
+			method: 'POST',
+			data: {
+				name: myName,
+				title: myTitle,
+				email: myEmail,
+				phone: myPhone,
+				twitterID: myTwitter
+			},
+			//http://api.jquery.com/Ajax_Events/
+		complete: function() {
 		},
-		//http://api.jquery.com/Ajax_Events/
-    complete: function() {
-    },
-    success: function (result) {
-      if(result.status) {
-					console.log('Result:' + result.status + ' Name:' + myName + ' Title:' + myTitle + ' Email:' + myEmail + ' Phone:' + myPhone + ' Twitter:' + myTwitter)
-					$.mobile.changePage("#home-page");
-      } else {
-					console.log('Contact Failed!')
-      }
-    },
-    error: function (request,error) {
-      // This callback function will trigger on unsuccessful action
-			console.log('Inside Error Function!' + error + request)
-		}
-	});
+		success: function (result) {
+		  if(result.status) {
+						console.log('Result:' + result.status + ' Name:' + myName + ' Title:' + myTitle + ' Email:' + myEmail + ' Phone:' + myPhone + ' Twitter:' + myTwitter)
+						$.mobile.changePage("#home-page");
+		  } else {
+						console.log('Add Contact Failed!')
+		  }
+		},
+		error: function (request,error) {
+		  // This callback function will trigger on unsuccessful action
+				console.log('Inside Error Function!' + error + request)
+			}
+		});
+	}
+	else {
+		
+			$.ajax({
+				url: 'http://contacts.tinyapollo.com/contacts/' + _contactID.trim() + '?key=' + apiKey,
+				method: 'PUT',
+				data: {
+				name: myName,
+				title: myTitle,
+				email: myEmail,
+				phone: myPhone,
+				twitterId: myTwitter
+			},
+			complete: function() {
+			},
+			success: function (result) {
+			  if(result.status=='success') {
+							console.log('Edit Result:' + ' Name:' + myName + ' Title:' + myTitle + ' Email:' + myEmail + ' Phone:' + myPhone + ' Twitter:' + myTwitter)				
+			  } else {
+							console.log('Edit Contact Failed!' + result + ' ' + result.message);
+			  }
+			  $.mobile.changePage("#home-page");
+			},
+			error: function (request,error) {
+			  // This callback function will trigger on unsuccessful action
+					console.log('Inside Error Function!' + error + request)
+				}
+			});
+	}
 });
